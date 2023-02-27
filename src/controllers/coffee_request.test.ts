1aa9a7ed99ac91d1ae93073677267a0222276fb5
glen <glen_dovey@hotmail.com>
2023-02-27 14:42:33 +0000

    added additional tests

--- src/controllers/coffee_request.test.ts
+++ src/controllers/coffee_request.test.ts
@@ -1,19 +1,42 @@
 import request from 'supertest'
 import { app } from '../app'
 
-describe('Test coffee API endpoint request', () => {
-    test('GET /coffeelover should return correct message', async () => {
+describe('Test /coffeelover API endpoint request', () => {
+    it('GET /coffeelover should return correct message', async () => {
         const res = await request(app).get('/coffeelover')
         expect(res.statusCode).toEqual(200)
         expect(res.text).toEqual('I like coffee!')
     })
 })
 
-test('GET /coffee should return correct object', async () => {
-    const res = await request(app).get('/coffee').query({ coffeeName: 'Latte' })
-    expect(res.statusCode).toEqual(200)
-    expect(res.body).toEqual({
-        drinkType: 'Coffee',
-        name: 'Latte',
+describe('Test /coffeelover API endpoint request', () => {
+    it('Should return correct object', async () => {
+        const res = await request(app)
+            .get('/coffee')
+            .query({ coffeeName: 'Latte' })
+        expect(res.statusCode).toEqual(200)
+        expect(res.body).toEqual({
+            drinkType: 'Coffee',
+            name: 'Latte',
+        })
+    })
+
+    it('Should return a valid latte coffee object with no params', async () => {
+        const res = await request(app).get('/coffee')
+        expect(res.statusCode).toEqual(200)
+        expect(res.body).toEqual({
+            drinkType: 'Coffee',
+            name: 'Latte',
+        })
+    })
+    it('Should return a valid macchiato coffee object', async () => {
+        const res = await request(app)
+            .get('/coffee')
+            .query({ coffeeName: 'Macchiato' })
+        expect(res.statusCode).toEqual(200)
+        expect(res.body).toEqual({
+            drinkType: 'Coffee',
+            name: 'Macchiato',
+        })
     })
 })
